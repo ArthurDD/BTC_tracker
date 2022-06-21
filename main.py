375ad3b32d6593a0e894c6164c07a14b09256495
Arthur <apd321@ic.ac.uk>
2022-06-21 14:09:25 +0100

    - Re-done the layer parsing method to be more exhaustive and work with the new data structure.

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ from chain_parser import *
 def main():
     # address = "bc1q6u5hsdjvz90fkzzmudz84jqtpdl0vc0yqd3375"
     # address = "1A1zP1eP5QGefi2DMPTfTL5SLmv7DivfNa"  # Satoshi's Address (50 mined BTC)
-    address = "115ZFznB6rTteLDF18AQTf2SWNBtoywoxb"  # Smaller address
+    # address = "115ZFznB6rTteLDF18AQTf2SWNBtoywoxb"  # Smaller address
+    address = "1MTKuR4CHJEZ1qSvoHyE8MDrVs4f1HQP5L"
 
     # try_scraper(address)
     try_parser(address)
