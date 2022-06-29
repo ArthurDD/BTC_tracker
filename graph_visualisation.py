e05c2d1ada766b0f3f6f657b50b82bb1a09fb773
Arthur <apd321@ic.ac.uk>
2022-06-29 11:50:29 +0100

    - Changed the library used to build graphs. Instead of using pygraphviz, we use directly graphviz and build the .dot file.

--- graph_visualisation.py
+++ graph_visualisation.py
@@ -1,31 +1,44 @@
 import pygraphviz as pgv
+import graphviz
 
 
 class GraphVisualisation:
     def __init__(self, transaction_lists):
         self.transaction_lists = transaction_lists
         self.depth = len(transaction_lists)
-        self.tree = pgv.AGraph()
+        self.dot = graphviz.Digraph('transaction-graph', comment='Transaction Graph', format='png')
+        self.dot.graph_attr['rankdir'] = 'LR'
         self.root_address = self.transaction_lists[0][0].output_addresses[0]
 
     def build_tree(self):
-        self.tree.add_node(self.root_address)
-        for layer in range(self.depth - 1):
-            if layer == 0:
-                for tx in self.transaction_lists[layer]:
-                    self.tree.add_edge(self.root_address, tx.txid)
-            elif layer == 1:
-                for tx in self.transaction_lists[layer]:
-                    for txx in self.transaction_lists[layer + 1]:
-                        if txx.prev_txid == tx.txid:
-                            self.tree.add_edge(tx.txid, txx.txid)
-        self.tree.layout()
-        self.tree.draw("graph.png")
+        self.dot.node_attr['shape'] = 'record'
+        self.dot.node('root', self.root_address)
+        # First, we add all the nodes to the graph
+        for layer in range(0, self.depth):
+            for tx in self.transaction_lists[layer]:
+                self.dot.node(tx.txid, label=rf"{tx.txid[:8]}...\n {tx.amount} BTC")
+
+        # Add edges between the root node (= input address) and its associated transactions
+        self.dot.edges(('root', add.txid) for add in self.transaction_lists[0])
+
+        # Add all the edges between the different layers.
+        for layer in range(1, self.depth):
+            for tx in self.transaction_lists[layer]:
+                for txx in self.transaction_lists[layer - 1]:
+                    if tx.prev_txid == txx.txid:
+                        self.dot.edge(txx.txid, tx.txid)
+        self.add_labels()
+        self.dot.render(directory='doctest-output', view=True)
         print("Tree done!")
 
-    def find_children(self, tx, layer_counter):
-        children = []
-        for txx in self.transaction_lists[layer_counter + 1]:
-            if txx.prev_txid == tx.id:
-                children.append(txx)
-        return children
+    def add_labels(self):
+        """
+        Changes the color of the identified nodes
+        :return: None
+        """
+        for layer in range(self.depth):
+            print(f"Layer: {layer}")
+            for tx in self.transaction_lists[layer]:
+                if tx.tag:
+                    print(f"Tag! {tx.tag} -- {tx.txid}")
+                    self.dot.node(tx.txid, color='red', style='filled', fillcolor='lightblue2')
