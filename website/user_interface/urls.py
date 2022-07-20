a9c88b169dfd021032bc86751bdcf6897365e9fd
Arthur <apd321@ic.ac.uk>
2022-07-20 23:55:37 +0100

    Removed unused link.

--- website/user_interface/urls.py
+++ website/user_interface/urls.py
@@ -4,6 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name="home_page"),
     path('start_tracking/', views.start_tracking, name='start_tracking'),
-    # path('trigger_message/', views.trigger_message, name='trigger_message'),
     path('display_graph/', views.display_graph, name="display_graph"),
 ]
