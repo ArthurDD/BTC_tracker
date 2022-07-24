a8baa4344f535b8b02cb41a89d060be764a86d6f
Arthur <apd321@ic.ac.uk>
2022-07-24 23:39:09 +0100

    - Commented code that is not used anymore, but might be useful in the future.

--- website/user_interface/consumers.py
+++ website/user_interface/consumers.py
@@ -1,57 +1,58 @@
 import json
-import pprint
-import string
-import random
+# import pprint
+# import string
+# import random
 import time
 from functools import partial
 
-from channels.generic.websocket import AsyncJsonWebsocketConsumer, WebsocketConsumer
-from asgiref.sync import async_to_sync
+from channels.generic.websocket import WebsocketConsumer
+# from channels.generic.websocket import AsyncJsonWebsocketConsumer
+# from asgiref.sync import async_to_sync
 
 from chain_parser import ChainParser
 from graph_visualisation import GraphVisualisation
 
 
-class ComplexUserInterfaceConsumer(AsyncJsonWebsocketConsumer):
-    async def connect(self):
-        letters = string.ascii_lowercase
-        result_str = ''.join(random.choice(letters) for i in range(20))
-        self.room_group_name = result_str  # 'test'
-
-        print(f"User: {self.room_group_name}")
-
-        await self.channel_layer.group_add(
-            self.room_group_name,
-            self.channel_name
-        )
-
-        await self.accept()
-
-    async def receive(self, text_data):
-        text_data_json = json.loads(text_data)
-        message = text_data_json['message']
-
-        await self.channel_layer.group_send(
-            self.room_group_name,
-            {
-                'type': 'chat_message',
-                'message': message
-            }
-        )
-
-    async def chat_message(self, event):
-        message = event['message']
-
-        await self.send(text_data=json.dumps({
-            'type': 'chat',
-            'message': message
-        }))
-
-    async def disconnect(self, close_code):
-        await self.channel_layer.group_discard(
-            self.room_group_name,
-            self.channel_name
-        )
+# class ComplexUserInterfaceConsumer(AsyncJsonWebsocketConsumer):
+#     async def connect(self):
+#         letters = string.ascii_lowercase
+#         result_str = ''.join(random.choice(letters) for i in range(20))
+#         self.room_group_name = result_str  # 'test'
+#
+#         print(f"User: {self.room_group_name}")
+#
+#         await self.channel_layer.group_add(
+#             self.room_group_name,
+#             self.channel_name
+#         )
+#
+#         await self.accept()
+#
+#     async def receive(self, text_data):
+#         text_data_json = json.loads(text_data)
+#         message = text_data_json['message']
+#
+#         await self.channel_layer.group_send(
+#             self.room_group_name,
+#             {
+#                 'type': 'chat_message',
+#                 'message': message
+#             }
+#         )
+#
+#     async def chat_message(self, event):
+#         message = event['message']
+#
+#         await self.send(text_data=json.dumps({
+#             'type': 'chat',
+#             'message': message
+#         }))
+#
+#     async def disconnect(self, close_code):
+#         await self.channel_layer.group_discard(
+#             self.room_group_name,
+#             self.channel_name
+#         )
 
 
 class UserInterfaceConsumer(WebsocketConsumer):
